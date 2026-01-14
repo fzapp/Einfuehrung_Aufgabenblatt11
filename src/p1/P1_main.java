746ec94256bcb9185103c79fd121ca35c05b6e2b
fzapp <fzapp@smail.uni-loeln.de>
2026-01-14 12:49:10 +0100

    aenderung_Einfuehrung_Aufgabenblatt11_Aufg1

---
+++ src/p1/P1_main.java
@@ -0,0 +1,37 @@
+package p1;
+
+public class P1_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public double potenzenRekursiv(double a, int b) {
+		if(b == 0) {
+			return 1D;
+		}
+		if(b < 0) {
+			return 1D/potenzenRekursiv(a, -b);
+		}
+		if(b == 1) {
+			return a;
+		}
+		return potenzenRekursiv(a, b-1);
+	}
+	
+	public double potenzenIterativ(double a, int b) {
+		if(b == 0) {
+			return 1D;
+		}
+		if(b < 0) {
+			a = 1D/a;
+		}
+		while(b > 1) {
+			a = a*a;
+			b--;
+		}
+		return a;
+	}
+
+}
